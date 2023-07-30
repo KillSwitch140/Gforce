2d7a461d86e3364eb665bf720f7f99920dce53b2
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-07-30 13:14:37 +0530

    Update Gforce.py

--- Gforce.py
+++ Gforce.py
@@ -11,6 +11,7 @@ def generate_response(uploaded_file, openai_api_key, query_text):
     if uploaded_file is not None:
         loader = PyPDFLoader(uploaded_file)
         documents = loader.load()
+        print(len(documents))
         # Split documents into chunks
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         texts = text_splitter.create_documents(documents)
