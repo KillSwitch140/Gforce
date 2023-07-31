9d3e03e1ad79892f2f2b897abfd9ae6ca66ae451
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-08-01 00:46:44 +0530

    Update Gforce.py

--- Gforce.py
+++ Gforce.py
@@ -47,15 +47,15 @@ def extract_experience(text):
 # Function to extract candidate name using GPT-3.5-turbo model
 def extract_candidate_name(resume_text):
     prompt = f"Please provide your full name:"
-    response = openai.Completion.create(
+    response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        prompt=prompt,
-        temperature=0,
-        max_tokens=100,
-        stop=["\n"],
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": resume_text},
+        ],
         api_key=openai_api_key
     )
-    candidate_name = response['choices'][0]['text'].strip()
+    candidate_name = response['choices'][0]['message']['content'].strip()
     return candidate_name
 
 # Page title and styling
