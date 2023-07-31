9351f72b35a42fdbc7d8077c5e4cc54c05f8cfc7
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-07-31 17:53:21 +0530

    Update Gforce.py

--- Gforce.py
+++ Gforce.py
@@ -11,51 +11,71 @@ from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from PyPDF2 import PdfReader
 
+openai_api_key = "OPENAI_API_KEY"
 
 def read_pdf_text(uploaded_file):
-    pdf_reader = PdfReader(uploaded_file)
+    pdf_reader = PyPDF2.PdfReader(uploaded_file)
     text = ""
 
     for page in pdf_reader.pages:
         text += page.extract_text()
 
     return text
-def generate_response(uploaded_file, openai_api_key, query_text):
-    # Load document if file is uploaded
-    if uploaded_file is not None:
-        documents = [read_pdf_text(uploaded_file)]
-        # Split documents into chunks
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        texts = text_splitter.create_documents(documents)
-        # Select embeddings
-        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-        # Create a vectorstore from documents
-        db = Chroma.from_documents(texts, embeddings)
-        # Create retriever interface
-        retriever = db.as_retriever()
-        # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-        return qa.run(query_text)
 
-# Page title
-st.set_page_config(page_title='GForce Resume Reader')
+# Page title and styling
+st.set_page_config(page_title='GForce Resume Reader', layout='wide')
 st.title('GForce Resume Reader')
 
 # File upload
-uploaded_file = st.file_uploader('Please upload you resume', type='pdf')
-# Query text
-query_text = st.text_input('How can I help?:', placeholder = 'Ask away!', disabled=not uploaded_file)
+uploaded_file = st.file_uploader('Please upload your resume', type='pdf')
+
+# Retrieve or initialize conversation history using SessionState
+if 'conversation_history' not in st.session_state:
+    st.session_state.conversation_history = []
+
+# Read the PDF content and set it as the initial context for the chatbot
+if uploaded_file is not None:
+    initial_context = read_pdf_text(uploaded_file)
+    st.session_state.conversation_history = [{'role': 'system', 'content': initial_context}]
+
+# User query
+query_text = st.text_input('You (Type your message here):', value='', help='Ask away!', type='default')
 
 # Form input and query
-result = []
 with st.form('myform', clear_on_submit=True):
-    openai_api_key =  st.secrets["OPENAI_API_KEY"]
-    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
-    if submitted and openai_api_key.startswith('sk-'):
-        with st.spinner('Loading response...'):
-            response = generate_response(uploaded_file, openai_api_key, query_text)
-            result.append(response)
-            del openai_api_key
+    st.form_submit_button('Send', help='Click to submit the query')
+    if query_text.strip() != '':
+        with st.spinner('Chatbot is typing...'):
+            # Add the user query to the conversation history
+            st.session_state.conversation_history.append({'role': 'user', 'content': query_text})
+            # Get the updated conversation history
+            conversation_history = st.session_state.conversation_history.copy()
+            # Generate the response using the updated conversation history
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=conversation_history,
+                api_key=openai_api_key
+            )
+            # Get the assistant's response
+            assistant_response = response['choices'][0]['message']['content']
+            # Append the assistant's response to the conversation history
+            st.session_state.conversation_history.append({'role': 'assistant', 'content': assistant_response})
 
-if len(result):
-    st.info(response)
+# Display the entire conversation history with chat bubbles
+if st.session_state.conversation_history:
+    st.header('Conversation History:')
+    chat_history_placeholder = st.empty()  # Placeholder to store chat history
+
+    for message in st.session_state.conversation_history:
+        if message['role'] == 'user':
+            st.markdown(f'<div style="display: flex; justify-content: flex-start; margin-bottom: 5px;">'
+                        f'<div style="display: block; padding: 5px; background-color: #e0e0e0; border-radius: 10px; width: 50%;">{message["content"]}</div>'
+                        f'</div>', unsafe_allow_html=True)
+        elif message['role'] == 'assistant':
+            st.markdown(f'<div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">'
+                        f'<div style="display: block; padding: 5px; background-color: #0078d4; color: white; border-radius: 10px; width: 50%; margin-left: 50%;">{message["content"]}</div>'
+                        f'</div>', unsafe_allow_html=True)
+
+# Add a clear conversation button
+if st.button('Clear Conversation'):
+    st.session_state.conversation_history.clear()
