0b392a5d53394c15d1eef8330b0020573c820f7e
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-08-01 18:15:43 +0530

    Update Gforce.py

--- Gforce.py
+++ Gforce.py
@@ -39,18 +39,19 @@ def extract_email(text):
     email_match = re.search(email_pattern, text)
     return email_match.group() if email_match else None
 
-# Function to extract candidate name using spaCy NER
-def extract_candidate_name(resume_text,max_length=50):
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(resume_text)
-    candidate_name = None
-    for ent in doc.ents:
-        if ent.label_ == "PERSON":
-            candidate_name = ent.text
-            break
+# Function to extract candidate name using GPT-3's prompt with a limited length
+def extract_candidate_name(resume_text, max_length=50):
+    prompt = f"Please provide your full name:"
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": resume_text[:max_length]},
+        ],
+        api_key=openai_api_key
+    )
+    candidate_name = response['choices'][0]['message']['content'].strip()
     return candidate_name
-
-
 
 # Page title and styling
 st.set_page_config(page_title='GForce Resume Reader', layout='wide')
@@ -69,7 +70,7 @@ if uploaded_files:
         if uploaded_file is not None:
             resume_text = read_pdf_text(uploaded_file)
             uploaded_resumes.append(resume_text)
-            # Extract GPA, email, and past 
+            # Extract GPA, email, and past experience
             gpa = extract_gpa(resume_text)
             email = extract_email(resume_text)
             # Extract candidate name using GPT-3's prompt with a limited length
@@ -90,9 +91,6 @@ if candidates_info:
         st.sidebar.markdown(f'<div style="display:flex"><div style="width: 100px; font-weight: bold;">Name:</div><div>{candidate_info["name"]}</div></div>', unsafe_allow_html=True)
         st.sidebar.markdown(f'<div style="display:flex"><div style="width: 100px; font-weight: bold;">GPA:</div><div>{candidate_info["gpa"]}</div></div>', unsafe_allow_html=True)
         st.sidebar.markdown(f'<div style="display:flex"><div style="width: 100px; font-weight: bold;">Email:</div><div>{candidate_info["email"]}</div></div>', unsafe_allow_html=True)
-        st.sidebar.markdown(f'<div style="display:flex"><div style="width: 100px; font-weight: bold;">Past Experience:</div><div>{candidate_info["experience"]}</div></div>', unsafe_allow_html=True)
-
-
 # User query
 user_query = st.text_area('You (Type your message here):', value='', help='Ask away!', height=100, key="user_input")
 
