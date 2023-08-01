01b944184d03e458c7e4216bf3d8cf059affa205
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-08-01 13:48:15 +0530

    Update Gforce.py

--- Gforce.py
+++ Gforce.py
@@ -41,17 +41,17 @@ def extract_email(text):
 
 # Function to extract past experience using GPT-3's prompt
 def extract_experience(resume_text,max_token=25):
-    prompt = f"Give a one line brief about this candidate "
+    prompt = f"Give brief of the candidate in one line"
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": prompt},
             {"role": "user", "content": resume_text},
         ],
-        api_key=openai_api_key,
+        api_key=openai_api_key
     )
-   experience_summary = response['choices'][0]['message']['content'].strip()
-    return experience_summary
+    experience = response['choices'][0]['message']['content'].strip()
+    return experience
 
 
 # Function to extract candidate name using spaCy NER
