bbe563e55943ac4bcddf4cd21f0ac36a6213c1aa
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2023-08-01 12:53:13 +0530

    Update Gforce.py

--- Gforce.py
+++ Gforce.py
@@ -40,8 +40,8 @@ def extract_email(text):
     return email_match.group() if email_match else None
 
 # Function to extract past experience using GPT-3's prompt
-def extract_experience(resume_text):
-    prompt = f"Extract and summarize past relevant professional experiences in 300 characters "
+def extract_experience(resume_text,,max_length=75):
+    prompt = f"Extract and summarize entire professional experience in 300 characters "
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
